a5c235deec31fc7ee9893f3c2ac9399855507a4a
Mohammed Areef <areefmohd2003@gmail.com>
2024-11-05 12:47:10 +0530

    areef

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -9,10 +9,10 @@ function Home() {
             <h2>Welcome to the Home Page</h2>
             <p>This is a protected page accessible after login.</p>
             <Carousel showArrows={true} autoPlay={true} infiniteLoop={true}>
-                <div><img src="image1.png" alt="Image 1" /></div>
-                <div><img src="image2.png" alt="Image 2" /></div>
-                <div><img src="image3.png" alt="Image 3" /></div>
-                <div><img src="image4.png" alt="Image 4" /></div>
+                <div><img src="https://images.pexels.com/photos/12904838/pexels-photo-12904838.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load" alt="Image 1" /></div>
+                <div><img src="https://images.pexels.com/photos/28905425/pexels-photo-28905425/free-photo-of-charming-spanish-architecture-in-sunlit-alley.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load" alt="Image 2" /></div>
+                <div><img src="https://images.pexels.com/photos/22719433/pexels-photo-22719433/free-photo-of-analog-camera-lying-in-front-of-a-computer-keyboard.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load" alt="Image 3" /></div>
+                <div><img src="https://images.pexels.com/photos/17504247/pexels-photo-17504247/free-photo-of-bag-and-book-with-poems-on-picnic-blanket.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load" alt="Image 4" /></div>
             </Carousel>
         </div>
     );
